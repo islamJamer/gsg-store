4abd0e8c892b02c4a5d8cf6360e970a280b74c4d
islamJomaa <77488050+islamJamer@users.noreply.github.com>
2022-01-04 21:28:31 +0200

    Create categories table

---
+++ database/migrations/2022_01_04_184457_create_categories_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('parent_id')
+                ->nullable()
+                ->constrained('categories', 'id')
+                ->nullOnDelete()
+                ->onUpdate('set null');
+
+            $table->string('name');
+            $table->string('slug')->unique();
+
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
